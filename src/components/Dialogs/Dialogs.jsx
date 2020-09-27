fee5c4d039a9be143ff1ee8119ea052db2a9ea8e
kurochkin-dev <kurochkindima.777@gmail.com>
2020-09-27 22:50:42 +0300

    refactoring the code

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
+        <div className={}>
             Dialogs
         </div>
     );
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
+import styles from './Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div>
+            <ProfileInfo />
+            <div>
+                <img className={styles.profile__img} src="https://www.w3schools.com/howto/img_snow_wide.jpg"></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts/>
+        </div>
+    );
+}
+
+export default Profile;
