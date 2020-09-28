d7e1e7a91c09d35795448e8a1a9baf5d23121c86
kurochkin-dev <kurochkindima.777@gmail.com>
2020-09-29 01:36:21 +0300

    refactor

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
-
-const DialogItem = (props) => {
-    return (
-        <div className={styles.dialog + ' ' + styles.active}>
-            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
-        </div>
-    );
-}
-
-const Message = (props) => {
-    return (
-        <div className={styles.message}>{props.message}</div>
-    );
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,7 +6,9 @@ const MyPosts = (props) => {
 
     let posts = [
         {id: 1, message: 'Hi, how are you?', likesCount: 12},
-        {id: 2, message: 'It\'s my first post', likesCount: 7}
+        {id: 2, message: 'It\'s my first post', likesCount: 7},
+        {id: 3, message: 'Bla', likesCount: 11},
+        {id: 4, message: 'Yes', likesCount: 5}
     ];
 
     let PostsElements =
